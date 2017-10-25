83a5bb0dede0d75ff68a48a7f5e277fa2195618b
kubas1129 <kubas11513@gmail.com>
2017-10-25 20:58:04 +0200

    Add complete solution for ROT11

--- Lab4/src/Exercise4Package/CryptMain.java
+++ Lab4/src/Exercise4Package/CryptMain.java
@@ -4,7 +4,6 @@ package Exercise4Package;
 public class CryptMain {
     public static void main(String[] arg)
     {
-        ROT11 r = new ROT11();
-        System.out.println(r.decrypt(r.crypt("abcdefghijklmnopqrtsuvwxyz")));
+
     }
 }
