5117f65bb55efefc7bc8371e4729200658bc1bc3
kubas1129 <kubas11513@gmail.com>
2017-10-25 23:49:06 +0200

    Finish crypto exercise

--- Lab4/src/Exercise4Package/CryptMain.java
+++ Lab4/src/Exercise4Package/CryptMain.java
@@ -1,9 +1,43 @@
 package Exercise4Package;
 
 
+import java.util.Scanner;
+
 public class CryptMain {
     public static void main(String[] arg)
     {
 
+        boolean exit = false;
+        while(!exit)
+        {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("1. Crypt File");
+            System.out.println("2. Decrypt File");
+            System.out.println("3. Exit");
+
+            int option = Integer.parseInt(sc.nextLine());
+
+            if(option == 1 || option == 2) {
+                System.out.println("1. ROT11 method");
+                System.out.println("2. Polibius method");
+                int method = Integer.parseInt(sc.nextLine());
+                if (method == 1 || method == 2)
+                {
+                    if(option == 1 && method == 1)
+                        Cryptographer.cryptfile(arg[1], arg[2], new ROT11());
+                    if(option == 1 && method == 2)
+                        Cryptographer.decryptfile(arg[1], arg[2], new Polibius());
+                    if(option == 2 && method == 1)
+                        Cryptographer.cryptfile(arg[1],arg[2], new ROT11());
+                    if(option == 2 && method == 2)
+                        Cryptographer.decryptfile(arg[1], arg[2], new Polibius());
+                }
+                else
+                    exit = true;
+            }
+            else
+                exit = true;
+        }
     }
 }
--- Lab4/src/Exercise4Package/Cryptographer.java
+++ Lab4/src/Exercise4Package/Cryptographer.java
@@ -9,9 +9,18 @@ interface Algorithm{
 
 
 public class Cryptographer {
+
     public static void cryptfile(String fileToCrypt, String fileToSave, Algorithm algo)
     {
+        String content = RW.ReadFromFile(fileToCrypt);
+        String crypted = algo.crypt(content);
+        RW.SaveToFile(fileToSave, crypted);
+    }
 
-
+    public static void decryptfile(String fileToDecrypt, String fileToSave, Algorithm algo)
+    {
+        String content = RW.ReadFromFile(fileToDecrypt);
+        String decrypted = algo.decrypt(content);
+        RW.SaveToFile(fileToSave, decrypted);
     }
 }
