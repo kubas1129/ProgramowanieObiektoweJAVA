4feefb8e8ebb12e30925009ad6dccd26cc42ae86
kubas1129 <kubas11513@gmail.com>
2017-11-29 23:36:11 +0100

    Finish lab6 exercises

--- Lab6/src/DrawShapesPackage/shapeWindowController.java
+++ Lab6/src/DrawShapesPackage/shapeWindowController.java
@@ -1,16 +1,15 @@
 package DrawShapesPackage;
 
-import ShapePackage.Circle;
-import ShapePackage.Shape;
-import javafx.event.EventType;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 /**
  * Created by kubas on 24.11.2017.
@@ -19,23 +18,72 @@ public class shapeWindowController {
 
     @FXML
     private Pane panePanel;
+    @FXML
+    private Shape shape;
+
+    private double orgMouseX, orgMouseY;
+    private double orgShapeX, orgShapeY;
+
+    private EventHandler<MouseEvent> onShapeMousePressedHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            orgMouseX = event.getSceneX();
+            orgMouseY = event.getSceneY();
+            orgShapeX = ((Shape)(event.getSource())).getTranslateX();
+            orgShapeY = ((Shape)(event.getSource())).getTranslateY();
+        }
+    };
+
+    private EventHandler<MouseEvent> onShapeMouseDragHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            double offsetX = event.getSceneX() - orgMouseX;
+            double offsetY = event.getSceneY() - orgMouseY;
+            double newTranslateX = orgShapeX + offsetX;
+            double newTranslateY = orgShapeY + offsetY;
+
+            ((Shape)(event.getSource())).setTranslateX(newTranslateX);
+            ((Shape)(event.getSource())).setTranslateY(newTranslateY);
+        }
+    };
+
+    private void DrawShape(Shape sh)
+    {
+        if(shape != null) panePanel.getChildren().remove(shape);
+        shape = sh;
+        shape.setOnMousePressed(onShapeMousePressedHandler);
+        shape.setOnMouseDragged(onShapeMouseDragHandler);
+        panePanel.getChildren().add(shape);
+    }
+
 
     public void DrawRect()
     {
-        Rectangle rect = new Rectangle(100,100,new Color(0.2,0.3,0.5,1.0));
-        panePanel.getChildren().add(rect);
+        DrawShape(new Rectangle(100,200,new Color(0.2,0.3,0.5,1.0)));
     }
 
-    public void OnDragShape()
+    public void DrawSquare()
     {
-        System.out.println("drag");
-        if(panePanel.getChildren().size() > 0)
-        {
-            Node nd = panePanel.getChildren().get(0);
-            nd.setTranslateX(nd.getTranslateX()+2);
-
-        }
+        DrawShape(new Rectangle(100,100,new Color(0.2,0.5,0.7,1.0)));
     }
+
+    public void DrawTriangle()
+    {
+        Polygon pl = new Polygon();
+        pl.getPoints().addAll(new Double[]{
+                10.0, 10.0,
+                10.0, 200.0,
+                250.0, 200.0,
+        });
+        pl.setFill(new Color(0.5412, 0, 1, 1));
+        DrawShape(pl);
+    }
+
+    public void DrawRound()
+    {
+        DrawShape(new Circle(50.0,50.0,50.0,new Color(0.2431, 0.702, 0.1216, 1)));
+    }
+
 
 
     void initialize()
---
+++ Lab6/src/PlotsPackage/PlotsMain.java
@@ -0,0 +1,25 @@
+package PlotsPackage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PlotsMain extends Application {
+
+    public static void main(String[] arg) {launch(arg);}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(this.getClass().getResource("plots.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Plots");
+        primaryStage.show();
+
+    }
+}
