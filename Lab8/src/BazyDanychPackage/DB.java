3ba13c2d7ab19897afa8f0bbc8fb086f2a308525
kubas1129 <kubas11513@gmail.com>
2017-12-07 22:10:06 +0100

    working on data base 2

--- Lab8/src/BazyDanychPackage/DB.java
+++ Lab8/src/BazyDanychPackage/DB.java
@@ -1,18 +1,22 @@
 package BazyDanychPackage;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kubas on 01.12.2017.
  */
 public class DB {
 
-    private Connection conn = null;
-    private Statement stat = null;
-    private ResultSet res = null;
+    private static Connection conn = null;
+    private  static Statement stat = null;
+    private static ResultSet res = null;
 
-    private String login = "";
-    private String pass = "";
+    private static String login = "";
+    private static String pass = "";
+
+    public static boolean connected = false;
 
     public DB(String username, String password)
     {
@@ -20,7 +24,14 @@ public class DB {
         pass = password;
     }
 
-    public void connect()
+    public static void ConnectToDB(String username, String password)
+    {
+        login  =username;
+        pass = password;
+        connect();
+    }
+
+    private static void connect()
     {
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -33,23 +44,66 @@ public class DB {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            connected = false;
+            return;
         }
-        catch (Exception e) {e.printStackTrace();}
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            connected = false;
+            return;
+        }
+        connected =true;
     }
 
 
-    public void listNames()
-    {
-        try
-        {
+    public static void listNames() {
+        try {
             connect();
             stat = conn.createStatement();
 
             res = stat.executeQuery("SELECT title FROM books");
 
+            while (res.next()) {
+                System.out.println(res.getString(1));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (res != null) {
+                try {
+                    res.close();
+                } catch (SQLException sqex) {
+                    sqex.printStackTrace();
+                }
+                res = null;
+            }
+
+            if (stat != null) {
+                try {
+                    stat.close();
+                } catch (SQLException sx) {
+                    sx.printStackTrace();
+                }
+                stat = null;
+            }
+        }
+    }
+
+
+    public static ArrayList<String> listFound(String name)
+    {
+        ArrayList<String> ls = new ArrayList<>();
+        try
+        {
+            connect();
+            stat = conn.createStatement();
+
+            res = stat.executeQuery("SELECT * FROM books WHERE author='" + name + "'");
+
             while(res.next())
             {
-                System.out.println(res.getString(1));
+                ls.add(res.getString(2));
             }
         } catch (SQLException ex) {ex.printStackTrace();}
 
@@ -73,9 +127,11 @@ public class DB {
             }
         }
 
+        return ls;
     }
 
 
 
-
 }
+
+
